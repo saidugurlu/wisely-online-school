6e81bae333c841600d47e79d2ff508b544078d87
saidugurlu <ugurlumsaid@gmx.de>
2022-06-28 15:23:25 +0200

    temp folder transferred to views and public folders

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ import express from 'express';
  
 const app = express();
 const port = 3031;
+
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+
+//Middlewares
+app.use(express.static("public"));
  
 app.get('/', (req, res) => {
-    res.send("<h1>test</h1>");
+    res.status(200).send("<h1>test</h1>");
 });
 
 app.listen(port, () => {
