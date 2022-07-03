ec3fa982b21c83dc27efb246fbe9564c9a7a77f6
saidugurlu <ugurlumsaid@gmx.de>
2022-07-03 16:04:25 +0200

    model / category  and categoryController created

--- models/Course.js
+++ models/Course.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const slugify = require('slugify');
+const mongoose = require("mongoose");
+const slugify = require("slugify");
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
@@ -18,18 +18,22 @@ const CourseSchema = new Schema({
     default: Date.now,
   },
   slug: {
-    type:String,
-    unique: true
-  }
+    type: String,
+    unique: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
 });
 
-CourseSchema.pre('validate', function(next){
+CourseSchema.pre("validate", function (next) {
   this.slug = slugify(this.name, {
-    lower:true,
-    strict:true
-  })
+    lower: true,
+    strict: true,
+  });
   next();
-})
+});
 
-const Course = mongoose.model('Course', CourseSchema);
+const Course = mongoose.model("Course", CourseSchema);
 module.exports = Course;
