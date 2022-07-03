3ba2f195dbb2737237e3a5900b7aa38714df3164
saidugurlu <ugurlumsaid@gmx.de>
2022-07-03 15:53:31 +0200

    edit  courseRoute.js id => slug

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,5 +5,5 @@ const router = express.Router();
 
 router.route("/").post(courseController.createCourse);
 router.route("/").get(courseController.getAllCourses);
-router.route('/:id').get(courseController.getCourse);
+router.route('/:slug').get(courseController.getCourse);
 module.exports = router;
