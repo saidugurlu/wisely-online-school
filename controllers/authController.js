2eb6f48dd2d1f07c3ffa84acc554d235a26ba75a
saidugurlu <ugurlumsaid@gmx.de>
2022-08-09 15:32:59 +0200

    user login work

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,48 @@
-const User = require('../models/User');
-
+const User = require("../models/User");
+bcrypt = require("bcrypt"); //sifre gizlemeye yarayan eklenti
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       user,
     });
   } catch (error) {
     res.status(400).json({
-      status: 'fail',
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+// Neu version of mongoose is not work with below code (await and callback)
+
+    /*     await User.findOne({ email: email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            res.status(200).send("YOU ARE LOGGED IN");
+          }
+        });
+      }
+    }
+    ); */
+    const user = await User.findOne({ email: email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => { // compare is a function of bcrypt and check that password is same or not with user.password
+        if (same) {
+          res.status(200).send("YOU ARE LOGGED IN");
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
       error,
     });
   }
