aadb1795213612c98b002dce76ed900e42f9f672
saidugurlu <ugurlumsaid@gmx.de>
2022-08-16 15:40:16 +0200

    dashboard page work with personalization

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,7 +39,7 @@ exports.loginUser = async (req, res) => {
         if (same) {
           // User Sessions
           req.session.userID = user._id;
-          res.status(200).redirect("/");
+          res.status(200).redirect("/users/dashboard");
         }
       });
     }
@@ -54,5 +54,13 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect("/");
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id: req.session.userID});  
+  res.status(200).render("dashboard", {
+    page_name: "dashboard",
+    user,
   });
 };
